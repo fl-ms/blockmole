54b992e26868038eb14195b725c3fbaf728cc1ba
fl-ms <44432633+fl-ms@users.noreply.github.com>
2018-10-26 15:10:20 +0200

    Add files via upload

--- blockmole_0_1.py
+++ blockmole_0_1.py
@@ -16,10 +16,6 @@ def clear():
     os.system("cls")
     return
 
-def print_header():
-    # Prints the nice Blockmole menu header :)
-    print(figlet_format("Blockmole"))
-    return
 
 def get_address_info(address):
     # Requests a certain Bitcoinaddress via blockchain-API and returns a list
@@ -27,43 +23,80 @@ def get_address_info(address):
     response_list = json.loads(response.text)
     return response_list
 
-def pretty_table(cursor, casenumber):
-    # Takes the cursor and tablenumbers and puts the SQLite data into a nice pretty table
-    cursor.execute("SELECT * FROM %s" % casenumber)
-    prettytable = from_db_cursor(cursor)
-    return prettytable
 
 def user_show_existing():
-    files = filter(os.path.isfile, os.listdir(os.curdir))
-    files_db = [fi for fi in files if fi.endswith(".db")]
-    clear()
-    print("\n### Existing casefiles ###\n")
-    for i in files_db:
-        print(" - " + str(i))
+    # Scans the installationfolder for *db files and lists them
+    freeze = True
+    while freeze == True:
+        files = filter(os.path.isfile, os.listdir(os.curdir))
+        files_db = [fi for fi in files if fi.endswith(".db")]
+        clear()
+        print("\n### Existing userfiles ###\n")
+        for i in files_db:
+            print(" - " + str(i) + "")
+        print("\n[1] - Back to menu\n")
+        var_menu = user_prompt()
+        if var_menu == "1":
+            freeze = False
+        else:
+            print("Invalid entry! Please try again")
+            time.sleep(1)
+        
     return
 
+def user_prompt():
+    prompt = input("blockmole >> ")
+    return prompt
+
+
 def user_create(username):
+    # Creates a new SQLite DB
     connect = sqlite3.connect(str(username)+'.db')
-    cursor = connect.cursor()
+    connect.close()
     db_loaded = True
-    print("\n### Database loaded! ###\n")
-    time.sleep(1)
+    print("\n### Logged in! ###\n")
+    time.sleep(2)
+    
     return db_loaded
+
+def case_create(db_name, case_name):
+    connect = sqlite3.connect(str(db_name)+'.db')
+    cursor = connect.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS [%s] (
+                    address_number INTEGER PRIMARY KEY,
+                    address TEXT,
+                    date_added TEXT,
+                    has_transaction BOOLEAN,
+                    last_used FLOAT,
+                    comment TEXT);""" % case_name)
+    connect.commit()
+    connect.close()
+    print("Case successfully created!")
+    time.sleep(1)
+    return True
+
+
+def print_header():
+    # Prints the nice Blockmole menu header :)
+    print(figlet_format("Blockmole"))
+    return
 
 
 def print_state(db_loaded, case_loaded, db_name, case_name):
+    # Prints the small status screen in the menu
     if db_loaded == True:
-            print("     # User %s logged in!" % str(db_name))
+            print("#  User:      %s" % str(db_name))
     else:
-        print("     # No user active. Please load a userfile!")
+        print("#  No user active. Please load a userfile!")
     if case_loaded == True:
-        print("     # Case %s loaded and active!\n"  % str(case_name))
+        print("#  Case:      %s\n"  % str(case_name))
     else:
-        print("     # No case loaded. Please load a case!\n")
+        print("#  No case loaded. Please load a case!\n")
     return
 
 
 def print_menu(db_loaded, case_loaded):
+    # Prints the menu
     if db_loaded == False:
         print("[1] - Show existing userfiles")
         print("[2] - Create or load a userfile\n")
@@ -72,7 +105,7 @@ def print_menu(db_loaded, case_loaded):
 
     elif db_loaded == True and case_loaded == False:
         print("[1] - Show existing casefiles")
-        print("[2] - Create or edit a casefile\n")
+        print("[2] - Create or load a casefile\n")
         print("[0] - Exit Blockmole\n")
         
 
@@ -82,29 +115,68 @@ def print_menu(db_loaded, case_loaded):
         print("[3] - Update transactiondata")
         print("[4] - Show addresses with new TXs\n")
         print("[0] - Exit Blockmole\n")
+
     return
 
 
-def main():
+def pretty_table(cursor, casenumber):
+    # Takes the cursor and tablenumbers and puts the SQLite data into a nice pretty table
+    cursor.execute("SELECT * FROM %s" % casenumber)
+    prettytable = from_db_cursor(cursor)
+    return prettytable
 
+
+def main():
+    # Main
     program_active = True
-    db_loaded = True
-    case_loaded = True
-    db_name = "Matthias"
-    case_name = "123456_2018"
+    db_loaded = False
+    case_loaded = False
+    db_name = ""
+    case_name = ""
     clear()
 
     while program_active == True:
         clear()
         print_header()
-        print("\n")
         print("Welcome to Blockmole - a simple Bitcoinaddress Trackingtool\n")
-        print("    > Current status:")
+        print("> Current status:\n")
         print_state(db_loaded, case_loaded, db_name, case_name)
+        print("\n")
         print_menu(db_loaded, case_loaded)
         
-        user_prompt = input("blockmole >> ")   
+        var_menu = user_prompt()
 
+        # User menu
+        if var_menu == "1" and db_loaded == False:
+            print("\nExisting userfiles:")
+            user_show_existing()  
+        elif var_menu == "2" and db_loaded == False:
+            print("\nPlease enter a username: \n")
+            db_name = user_prompt()
+            db_loaded = user_create(db_name)
+
+        # Case menu
+        elif var_menu == "1" and db_loaded == True:
+            pass
+        elif var_menu == "2" and db_loaded == True:
+            print("\nPlease enter the casenumber: \n")
+            case_name = user_prompt()
+            case_loaded = case_create(db_name, case_name)
+
+        # Tracking menu
+        elif var_menu == "1" and db_loaded == True and case_loaded == True:
+            pass
+        elif var_menu == "2" and db_loaded == True and case_loaded == True:
+            pass
+        elif var_menu == "3" and db_loaded == True and case_loaded == True:
+            pass
+
+        elif var_menu == "0":
+            program_active = False
+        
+        else:
+            print("# Invalid entry. Please retry! #")
+            time.sleep(1)
     
     
 if __name__ == '__main__':
