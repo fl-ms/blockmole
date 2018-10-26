eb39de396808c2f639b8b56a3a4f8f4ef697019f
fl-ms <44432633+fl-ms@users.noreply.github.com>
2018-10-26 13:30:03 +0200

    Refactoring

--- blockmole_0_1.py
+++ blockmole_0_1.py
@@ -18,7 +18,6 @@ def clear():
 
 def print_header():
     # Prints the nice Blockmole menu header :)
-    clear()
     print(figlet_format("Blockmole"))
     return
 
@@ -51,44 +50,63 @@ def user_create(username):
     time.sleep(1)
     return db_loaded
 
-def menu_structure(menu_number, db_loaded, db_name, case_loaded, case_name):
-    clear()
-    print_header()
-    print("\n")
-    print("Welcome to Blockmole - a simple Bitcoinaddress Trackingtool\n")
-    print("    > Current status:")
+
+def print_state(db_loaded, case_loaded, db_name, case_name):
     if db_loaded == True:
-        print("     # User %s logged in!"%str(db_name))
+            print("     # User %s logged in!" % str(db_name))
     else:
         print("     # No user active. Please load a userfile!")
     if case_loaded == True:
-        print("     # Case %s loaded and active!\n"  % str (case_name))
+        print("     # Case %s loaded and active!\n"  % str(case_name))
     else:
         print("     # No case loaded. Please load a case!\n")
-
-    if menu_number == 0:
-        print("test")
-    
     return
 
+
+def print_menu(db_loaded, case_loaded):
+    if db_loaded == False:
+        print("[1] - Show existing userfiles")
+        print("[2] - Create or load a userfile\n")
+        print("[0] - Exit Blockmole\n")
+           
+
+    elif db_loaded == True and case_loaded == False:
+        print("[1] - Show existing casefiles")
+        print("[2] - Create or edit a casefile\n")
+        print("[0] - Exit Blockmole\n")
+        
+
+    elif db_loaded == True and case_loaded == True:
+        print("[1] - Show bitcoinaddresses in this case")
+        print("[2] - Add bitcoinaddress to this case")
+        print("[3] - Update transactiondata")
+        print("[4] - Show addresses with new TXs\n")
+        print("[0] - Exit Blockmole\n")
+    return
 
 
 def main():
+
     program_active = True
+    db_loaded = True
+    case_loaded = True
+    db_name = "Matthias"
+    case_name = "123456_2018"
+    clear()
+
     while program_active == True:
-                
-        menu_structure(0, True, "Matthias", False, "123456/2018")
-        user_prompt = input("blockmole >>")
-        if user_prompt == "2":
-            program_active = False
+        clear()
+        print_header()
+        print("\n")
+        print("Welcome to Blockmole - a simple Bitcoinaddress Trackingtool\n")
+        print("    > Current status:")
+        print_state(db_loaded, case_loaded, db_name, case_name)
+        print_menu(db_loaded, case_loaded)
+        
+        user_prompt = input("blockmole >> ")   
 
-    return
-
-
-
-
-
-
+    
+    
 if __name__ == '__main__':
     main()
 
